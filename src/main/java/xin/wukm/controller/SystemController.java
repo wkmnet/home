175cee9e144eae429262d9fe05b4d5b07d038622
Wukunmeng <wukunmeng@xinpianchang.com>
2017-06-24 17:30:06 +0800

    修改

--- src/main/java/xin/wukm/controller/SystemController.java
+++ src/main/java/xin/wukm/controller/SystemController.java
@@ -10,17 +10,11 @@
  */
 package xin.wukm.controller;
 
-import org.apache.commons.lang.time.DateFormatUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.management.ManagementFactory;
-
-import java.lang.management.OperatingSystemMXBean;
-import java.lang.management.ThreadMXBean;
-import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -42,21 +36,8 @@ public class SystemController extends BaseController {
     @RequestMapping(value = "/info",method = RequestMethod.GET)
     @ResponseBody
     public Object info(){
-        Map<String,Object> detail = new HashMap<String,Object>();
-        Runtime runtime = Runtime.getRuntime();
-        detail.put("totalMemory", runtime.totalMemory());
-        detail.put("maxMemory", runtime.maxMemory());
-        detail.put("freeMemory",runtime.freeMemory());
-        detail.put("availableProcessors", runtime.availableProcessors());
-        OperatingSystemMXBean osmb = ManagementFactory.getOperatingSystemMXBean();
-        detail.put("systemLoadAverage", osmb.getSystemLoadAverage());
-        detail.put("osName", osmb.getName());
-        detail.put("version", osmb.getVersion());
-        ThreadMXBean tmb = ManagementFactory.getThreadMXBean();
-        detail.put("threadCount", tmb.getThreadCount());
-        Calendar c = Calendar.getInstance();
-        detail.put("time", c.getTimeInMillis());
-        detail.put("dateTime", DateFormatUtils.ISO_DATETIME_FORMAT.format(c));
+        Map<String,Object> detail = new HashMap();
+        detail.put("time", System.currentTimeMillis());
         return detail;
     }
 
